1d8a4bad39d6467984944c8809d0a099fd896876
smarinacseas <smarinac@seas.upenn.edu>
2025-01-22 20:04:04 -0600

    Comments for clarity (purpose/steps of pipeline)

--- pipelines/digital_data_etl.py
+++ pipelines/digital_data_etl.py
@@ -3,9 +3,9 @@ from zenml import pipeline
 from steps.etl import crawl_links, get_or_create_user
 
 
-@pipeline
+@pipeline # Extract, Transform, and Load (ETL) pipeline
 def digital_data_etl(user_full_name: str, links: list[str]) -> str:
-    user = get_or_create_user(user_full_name)
-    last_step = crawl_links(user=user, links=links)
+    user = get_or_create_user(user_full_name) # Get/create user from/in database (Step 1)
+    last_step = crawl_links(user=user, links=links) # Crawl links (Step 2)
 
     return last_step.invocation_id
